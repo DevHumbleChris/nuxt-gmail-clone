8c97bb2c2689fd5c8affca8f237a1680977c3ca6
Am.ChrisKE <63234437+DevHumbleChris@users.noreply.github.com>
2023-06-02 16:10:13 +0000

    saving changes

--- components/Compose.vue
+++ components/Compose.vue
@@ -1,67 +1,77 @@
 <script setup>
-import { useComposeStore } from '~/stores/compose'
+import { useComposeStore } from "~/stores/compose";
 
-const composeStore = useComposeStore()
+const composeStore = useComposeStore();
 
 const isMinimize = computed(() => {
-  return composeStore.isMinimize
-})
+  return composeStore.isMinimize;
+});
 
 const isMaximize = computed(() => {
-  return composeStore.isMaximize
-})
+  return composeStore.isMaximize;
+});
 
 const isFullWidth = computed(() => {
-  return composeStore.isFullWidth
-})
+  return composeStore.isFullWidth;
+});
 
 const isComposingMail = computed(() => {
-  return composeStore.isComposingMail
-})
+  return composeStore.isComposingMail;
+});
 
 const maximize = () => {
-  composeStore.maximize()
-}
+  composeStore.maximize();
+};
 
 const minimize = () => {
-  composeStore.minimize()
-}
+  composeStore.minimize();
+};
 
 const composeMail = () => {
   composeStore.composeMail();
+};
+
+const fullWidth = () => {
+  composeStore.fullWidth();
 };
 </script>
 
 <template>
   <div
-  v-show="isComposingMail"
-    class="fixed right-0 w-full sm:h-[30rem] sm:w-[30rem] sm:absolute bottom-0 drop-shadow-2xl sm:right-16 text-sm shadow-lg shadow-cyan-500/200 rounded-t-lg z-30 overflow-hidden"
-    :class="{ 'w-[17rem]': isMinimize, 'w-[30rem] h-[30rem]': isMaximize }"
+    v-if="isComposingMail"
+    class="fixed right-0 w-full sm:w-[30rem] h-[30rem] drop-shadow-2xl sm:right-16 text-sm shadow-lg shadow-cyan-500/200 rounded-t-lg z-30 overflow-hidden"
+    :class="{
+      'w-[17rem] bottom-0 h-[2.5rem]': isMinimize,
+      'w-[30rem] h-[30rem]': isMaximize,
+      'bottom-28 sm:right-48 sm:w-[52rem]': isFullWidth,
+    }"
   >
     <div class="bg-[#f2f5fc] p-3 flex items-center justify-between">
       <p class="text-[#172846]">New Message</p>
       <div class="flex items-center space-x-2">
         <button @click="minimize" class="cursor-pointer">
-          <Icon
-            name="codicon:chrome-minimize"
-            class="w-5 h-auto"
-          />
+          <Icon name="codicon:chrome-minimize" class="w-5 h-auto" />
         </button>
-        <button class="cursor-pointer">
-          <Icon
-            name="ic:outline-open-in-full"
-            class="w-4 h-auto"
-          />
+        <button @click="fullWidth" class="cursor-pointer">
+          <Icon name="ic:outline-open-in-full" class="w-4 h-auto" />
         </button>
         <button @click="composeMail" class="cursor-pointer">
           <Icon name="ic:round-close" class="w-4 h-auto" />
         </button>
       </div>
     </div>
-    <div :class="{ 'bg-white h-full w-full px-3': !isMinimize, 'hidden': isMinimize }">
+    <div
+      :class="{
+        'bg-white h-full w-full px-3': !isMinimize,
+        'hidden': isMinimize,
+      }"
+    >
       <div class="flex items-center justify-between border-b border-gray-200">
         <p>To</p>
-        <input type="text" class="w-full outline-none border-0 focus:ring-0 bg-transparent">
+        <input
+          type="text"
+          class="w-full outline-none border-0 focus:ring-0 bg-transparent"
+        />
         <div class="flex items-center space-x-2">
           <button class="hover:underline">Cc</button>
           <button class="hover:underline">Bcc</button>
@@ -69,14 +79,20 @@ const composeMail = () => {
       </div>
       <div class="flex items-center justify-between border-b border-gray-200">
         <p>Cc</p>
-        <input type="text" class="w-full outline-none border-0 focus:ring-0 bg-transparent">
+        <input
+          type="text"
+          class="w-full outline-none border-0 focus:ring-0 bg-transparent"
+        />
         <div class="flex items-center space-x-2">
           <button class="hover:underline">Bcc</button>
         </div>
       </div>
       <div class="flex items-center justify-between border-b border-gray-200">
         <p>Bcc</p>
-        <input type="text" class="w-full outline-none border-0 focus:ring-0 bg-transparent">
+        <input
+          type="text"
+          class="w-full outline-none border-0 focus:ring-0 bg-transparent"
+        />
       </div>
     </div>
   </div>
--- stores/compose.js
+++ stores/compose.js
@@ -1,22 +1,30 @@
 export const useComposeStore = defineStore("compose", () => {
-
   const isComposingMail = useState("isComposingMail", () => false);
   const isMinimize = useState("isMinize", () => false);
-  const isMaximize = useState("isMaximize", () => true);
+  const isMaximize = useState("isMaximize", () => false);
   const isFullWidth = useState("isFullWidth", () => false);
 
   const maximize = () => {
-    isMaximize.value = !isMaximize.value
-    isMinimize.value = !isMinimize.value
-  }
-  
+    isMaximize.value = !isMaximize.value;
+    isFullWidth.value = !isFullWidth.value;
+  };
+
   const minimize = () => {
-    isMinimize.value = !isMinimize.value
-    isMaximize.value = !isMaximize.value
-  }
+    isMinimize.value = !isMinimize.value;
+  };
   const composeMail = () => {
-    isComposingMail.value = !isComposingMail.value
-  }
+    isComposingMail.value = !isComposingMail.value;
+    if (!isComposingMail.value) {
+      isMaximize.value = false;
+      isMinimize.value = false;
+      isFullWidth.value = false;
+    }
+  };
+
+  const fullWidth = () => {
+    isFullWidth.value = !isFullWidth.value;
+    isMaximize.value = !isMaximize.value;
+  };
 
   return {
     isComposingMail,
@@ -25,6 +33,7 @@ export const useComposeStore = defineStore("compose", () => {
     isFullWidth,
     maximize,
     minimize,
-    composeMail
+    composeMail,
+    fullWidth,
   };
 });
