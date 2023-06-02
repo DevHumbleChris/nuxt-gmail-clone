09f3fbfcfa8081b0c1da8657f1566fdad71e9411
Am.ChrisKE <63234437+DevHumbleChris@users.noreply.github.com>
2023-06-02 08:33:45 +0000

    modified sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -12,102 +12,103 @@ import {
 </script>
 
 <template>
-  <aside class="sticky top-0 w-[16rem] p-3 h-full">
+  <aside class="sticky top-0 w-[7rem] sm:w-[16rem] p-3 h-screen sm:bg-blue-700 group">
     <div class="flex space-x-2 items-center">
       <Bars3Icon class="w-6" />
       <img
         src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png"
+        class="hidden sm:block"
       />
     </div>
     <button
-      class="flex items-center space-x-2 bg-[#c3e7ff] p-4 my-2 rounded-2xl"
+      class="flex items-center space-x-2 bg-[#c3e7ff] p-2 sm:p-4 my-2 rounded-md sm:rounded-2xl"
     >
       <PencilIcon class="w-6 text-[#061d47]" />
-      <span>Compose</span>
+      <span class="hidden sm:block">Compose</span>
     </button>
-    <ul class="my-4 space-y-1">
+    <ul class="my-4 space-y-3 text-center">
       <li>
         <button class="flex items-center space-x-3">
-          <InboxIcon class="w-4" />
-          <span>Inbox</span>
+          <InboxIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Inbox</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <StarIcon class="w-4" />
-          <span>Starred</span>
+          <StarIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Starred</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <ClockIcon class="w-4" />
-          <span>Snoozed</span>
+          <ClockIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Snoozed</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="ic:round-send" class="w-4 h-auto" />
-          <span>Sent</span>
+          <Icon name="ic:round-send" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Sent</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <DocumentIcon class="w-4" />
-          <span>Drafts</span>
+          <DocumentIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Drafts</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <ChevronUpIcon class="w-4" />
-          <span>Less</span>
+          <ChevronUpIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Less</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="material-symbols:label-important-outline" class="w-4 h-auto" />
-          <span>Important</span>
+          <Icon name="material-symbols:label-important-outline" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Important</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="ic:outline-chat" class="w-4 h-auto" />
-          <span>Chats</span>
+          <Icon name="ic:outline-chat" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Chats</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="material-symbols:schedule-send-outline" class="w-4 h-auto" />
-          <span>Scheduled</span>
+          <Icon name="material-symbols:schedule-send-outline" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Scheduled</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="mdi:email-multiple-outline" class="w-4 h-auto" />
-          <span>All Mail</span>
+          <Icon name="mdi:email-multiple-outline" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">All Mail</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="ri:spam-2-line" class="w-4 h-auto" />
-          <span>Spam</span>
+          <Icon name="ri:spam-2-line" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Spam</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="material-symbols:delete-outline-rounded" class="w-4 h-auto" />
-          <span>Trash</span>
+          <Icon name="material-symbols:delete-outline-rounded" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Trash</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Icon name="material-symbols:label-outline-rounded" class="w-4 h-auto" />
-          <span>Categories</span>
+          <Icon name="material-symbols:label-outline-rounded" class="w-6 sm:w-4 h-auto" />
+          <span class="hidden sm:block">Categories</span>
         </button>
       </li>
       <li>
         <button class="flex items-center space-x-3">
-          <Cog6ToothIcon class="w-4" />
-          <span>Manage labels</span>
+          <Cog6ToothIcon class="w-6 sm:w-4" />
+          <span class="hidden sm:block">Manage labels</span>
         </button>
       </li>
     </ul>
