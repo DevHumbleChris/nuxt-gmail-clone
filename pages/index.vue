c97a84f2e3ba14fcda65df4201ff33f029a798f8
DevHumbleChris <dev.humblechris254@gmail.com>
2024-01-15 17:31:12 +0300

    realtime data working

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <script setup>
+import { collection } from "firebase/firestore";
 definePageMeta({
   title: "Inbox",
   middleware: ["auth"],
 });
+const db = useFirestore();
+const todos = useCollection(collection(db, "todos"));
+console.log(todos);
 </script>
 
 <template>
   <section>
     <LazyLabelsWrapper />
     <div>
-      <LazyMessage />
-      <LazyMessage />
+      <LazyMessage v-for="todo in todos" :key="todo.id" />
     </div>
   </section>
 </template>
